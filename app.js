2a5e2acfc5a4ebb2e47bf1263f067c4499bbf7ac
LeeHonggyo <ghdry2@gmail.com>
2023-11-19 13:32:18 +0900

    These are the initial settings for creating a server. We installed node and express, and load server values through RestAPI.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.set("port", port);
 
 app.get("/", (req, res) => {
-  res.send("This is Test!!");
+  res.send("Hello world!");
 });
 
 app.listen(port, () => console.log("Listening on", port));
